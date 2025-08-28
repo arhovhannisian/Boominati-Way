5a93c37688f86043d6e0cb5dc760fb4d731982c7
arhovhannisian <hovhannisianaram@gmail.com>
2025-08-28 15:47:43 +0400

    ashxatec about.jsx

--- src/Pages/PrivatePages/about.jsx
+++ src/Pages/PrivatePages/about.jsx
@@ -7,13 +7,6 @@ const About = () => {
     const [currentVideo, setCurrentVideo] = useState('drnBMAEA3AM');
     const videos = [
         {
-
-
-
-
-
-
-
             
             title: 'arrrrrj',
            src: 'WtuEDKXvJho'
